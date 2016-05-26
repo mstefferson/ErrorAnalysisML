273481a659c54f0b1dc598caefa25fd176eb06c6
Michael Stefferson <mstefferson@gmail.com>
2016-05-26 12:13:59 -0600

    Calculated sig using all the methods. It's more than I want the outputs, but this is just a copy that's useful for comparing everything.

--- TestFits.m
+++ TestFits.m
@@ -8,8 +8,8 @@ x   = linspace(-1,1,N)';
 y_exact = a * x + b;
 y_pert  = y_exact + 1 / 10 * randn( N, 1 );
 %sig = 1/1000 * ones(N,1); 
-sig = ones(N,1); 
-%sig = 1/20 * sqrt( (1:N) )';
+%sig = ones(N,1); 
+sig = 1/20 * sqrt( (1:N) )';
 
 % errorbar( x, y_pert, sig,'o' );
 
@@ -27,6 +27,20 @@ ft = fittype('poly1');
 fitmat = fit( x, y_pert, ft, fo);
 fitn = polyfitn(x, y_pert, 'constant, x' );
 
+% Display Everything
+
+fprintf('fitlin: uses sigma, my linear \n'); 
+disp(fitlin);
+fprintf('fitNoSig: no sigma, my linear \n');
+disp(fitNoSig);
+fprintf('fitpoly: uses sigma, my curvature matrix fit \n');
+disp(fitpoly);
+fprintf('poly: no sigma, uses polyfit\n');
+disp(poly);
+fprintf('fitmat: uses sigma, uses fit\n');
+disp(fitmat);
+fprintf('fitn: no sigma, uses polyfitn\n');
+disp(fitn);
 
 
 
--- lsfLin.m
+++ lsfLin.m
@@ -11,6 +11,9 @@
 
 function [fitobj, gof] = lsfLin(x,y,sig)
 
+% vector length
+N = length( x );
+
 % if no sig, we assume it's the same for all points
 if nargin == 2
   % Check that all vectors are the same length
@@ -25,7 +28,7 @@ if nargin == 2
   y_sig2_sum  = sum( y );
   x_sig2_sum  = sum( x );
   xy_sig2_sum = sum(  x .* y );
-  one_sig2_sum  = length(x);
+  one_sig2_sum  = N;
 else
   % Check that all vectors are the same length
   [xr, xc] = size( x );
@@ -58,20 +61,66 @@ if nargin == 3
   % Chi square
   gof.chiSq = sum( ( ( y - (a1 * x  + a0) ) ./ sig ) .^ 2 );
   gof.chiSq_red = gof.chiSq ./ ( length(x) - 2 ); % Reduced chiSq
-  % Uncertainties. Calucated from error propagation
-  %sig_a0 = sqrt( 1 ./ Delta * x2_sig2_sum );
-  %sig_a1 = sqrt( 1 ./ Delta * one_sig2_sum ); 
-  xprime = x - mean(x);
-  sig_a1 = sqrt( 1 ./ sum( xprime .^ 2 ) ); 
-  sig_a0 = sqrt( one_sig2_sum ); 
+
+  % Uncertainties. Calculated from error propagation
+  sig2_a0_ep =  1 ./ Delta * x2_sig2_sum ;
+  sig2_a1_ep =  1 ./ Delta * one_sig2_sum ; 
+
+  % Uncertainties. Calculated using Gaussian properties
+  aveXw  = x_sig2_sum / one_sig2_sum;
+  aveX2w = x2_sig2_sum / one_sig2_sum;
+
+  % Shifted x. Done to get uncorrelated errors
+  xprime = x - aveXw;
+
+  sig2_a1_g = 1 / sum( w .* xprime .^2 ) ;
+  sig2_a0p_g = 1 / one_sig2_sum ;
+  sig2_a0_g = sig2_a0p_g  +  aveXw .^ 2 * sig2_a1_g ;
 
 end
 
-gof.res  = sum( ( y - (a1 * x  + a0) )  .^ 2 ); 
+% Shifted y intercept, should be equal to <y>
+a0p = a0 + a1 .* x_sig2_sum / one_sig2_sum;
+
+% Calculate errors assuming all errors are equal- scatter of points
+sig2_guess = 1 / ( N - 2 ) .* sum( ( a0 + a1 .* x - y ) .^ 2 );
+aveXw  = sum(x) / N;
+xprime = x - aveXw ;
+
+sig2_a1_sp = sig2_guess  ./ sum( xprime .^ 2 ) ;
+sig2_a0p_sp = sig2_guess / N ;
+sig2_a0_sp = sig2_a0p_sp + aveXw .^ 2 * sig2_a1_sp ;
 
 % Put it in a struct
+
+% a0
 fitobj.a0 = a0;
-if nargin == 3; fitobj.sig_a0 = sig_a0; end
+if nargin == 3; 
+  fitobj.sig_a0_ep = sqrt( sig2_a0_ep ); 
+  fitobj.sig_a0_g = sqrt( sig2_a0_g ); 
+end
+  fitobj.sig_a0_sp = sqrt( sig2_a0_sp ); 
+
+% a0' (prime)
+fitobj.a0p = a0p;
+if nargin == 3; 
+  fitobj.sig_a0p_g = sqrt( sig2_a0p_g ); 
+end
+  fitobj.sig_a0p_sp = sqrt( sig2_a0p_sp ); 
+
+% a1  
 fitobj.a1 = a1;
-if nargin == 3; fitobj.sig_a1 = sig_a1; end
+if nargin == 3; 
+  fitobj.sig_a1_ep = sqrt( sig2_a1_ep ); 
+  fitobj.sig_a1_g = sqrt( sig2_a1_g ); 
+end
+  fitobj.sig_a1_sp = sqrt( sig2_a1_sp ); 
+
+  fitobj.sig_Chi2  = sqrt(sig2_guess);
+  
+
+
+% Residual
+gof.res  = sum( ( y - (a1 * x  + a0) )  .^ 2 ); 
+
 
